bd2d2b3bd18feb8b355f1b868e5290b6f988a8c6
Victor Hugo <victorhugorch@gmail.com>
2017-10-22 13:27:26 -0200

    create new permissions on seed

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -8,9 +8,14 @@ class PermissionsTableSeeder extends Seeder
     public function run()
     {
         $permissions = [
-            ['name' => 'full_access', 'description' => 'Has access to all Admin System.'],
-            ['name' => 'add_instagram_acc', 'description' => 'Has access to add new instagram acc.'],
-            ['name' => 'list_instagram_acc', 'description' => 'Has access to list all instagram acc on sys.'],
+            ['name' => 'full_access',  'description' => 'Has access to all Admin System.'],
+
+            ['name' => 'create_user',  'description' => 'Has access to create new users'],
+            ['name' => 'list_user',    'description' => 'Has access to list users on sys.'],
+            ['name' => 'disable_user', 'description' => 'Has access to disable users.'],
+
+            ['name' => 'add_instagram_acc',    'description' => 'Has access to add new instagram acc.'],
+            ['name' => 'list_instagram_acc',   'description' => 'Has access to list all instagram acc on sys.'],
             ['name' => 'delete_instagram_acc', 'description' => 'Has access to delete instagram acc on sys.'],
         ];
 
