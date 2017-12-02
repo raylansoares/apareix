5e40f80199fac8037596784e5b1443611f9c2bf7
Victor Hugo <victorhugorch@gmail.com>
2017-12-02 10:43:12 -0200

    working on seeds permissions

--- database/seeds/PermissionRoleTableSeeder.php
+++ database/seeds/PermissionRoleTableSeeder.php
@@ -11,6 +11,8 @@ class PermissionRoleTableSeeder extends Seeder
             // role 2 = Add instagram acc
             // role 3 = list instagram acc
             // role 4 = delete instagram acc
+            ['permission_id' => 1, 'role_id' => 1],
+
             ['permission_id' => 2, 'role_id' => 1],
             ['permission_id' => 3, 'role_id' => 1],
 
